49a2e79fe985be107e9d365e09b283d2f8f5053a
Andrade-Tales <selatedardna.1476@gmail.com>
2022-05-23 20:22:12 -0300

    Adicionado dependências do projeto

---
+++ src/test/java/br/com/safe4woman/S4F/S4FApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.safe4woman.S4F;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4FApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
